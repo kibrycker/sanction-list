9b179d44b5600f3be31c2047772d1b9b6e5a10a8
maxim.martyskin <maxim.martyshkin@kartoteka.ru>
2022-10-22 09:08:29 +0300

    Правка настроек webpack и файлов с ним связанных. Правка подключения assets.

--- src/Controller/OrganizationController.php
+++ src/Controller/OrganizationController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/sanction-list/admin/organization')]
+#[Route('/admin/organization')]
 class OrganizationController extends AbstractController
 {
     /** @var int Лимит выводимого списка */
