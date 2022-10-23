1fe0afd06019a6f45020ccfa5ba5b59761ed821a
maxim.martyskin <maxim.martyshkin@kartoteka.ru>
2022-10-23 21:02:31 +0300

    Поправил определение базы данных для юзера, имена урлов для авторизации/деавторизации. Добавил форму, для авторизации юзера

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,17 +6,42 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
 
-    #[Route('/login', name: 'security_login')]
-    public function login(Request $request): Response
+    /**
+     * Страница авторизации
+     *
+     * @param Request $request Запрос
+     * @param AuthenticationUtils $helper
+     *
+     * @return Response
+     */
+    #[Route('/login', name: 'sanction_list_security_login')]
+    public function login(Request $request, AuthenticationUtils $helper): Response
     {
-        return $this->render('security/login.html.twig', []);
+        if ($user = $this->getUser()) {
+            /** Проверяем на админа, для определения куда кого кидать */
+            $isAdmin = in_array('ROLE_ADMIN', $user->getRoles());
+            $routes = 'sanction_list_' . ($isAdmin ? 'admin_' : '') . 'organization_index';
+            return $this->redirectToRoute($routes);
+        }
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $helper->getLastUsername(),
+            'error' => $helper->getLastAuthenticationError(),
+        ]);
     }
 
-    #[Route('/logout', name: 'security_logout')]
+    /**
+     * Страница деавторизации
+     *
+     * @return void
+     * @throws \Exception
+     */
+    #[Route('/logout', name: 'sanction_list_security_logout')]
     public function logout(): void
     {
         throw new \Exception('This should never be reached!');
